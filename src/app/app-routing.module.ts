d3dffc9b301819db95cccc985d2e333b311162db
Jenny <yevgeniia.shtompel@gmail.com>
2020-11-30 16:23:47 +0100

    changeHeader

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AktuellesComponent } from './aktuelles/aktuelles.component';
-import { UnserTeamComponent } from './unser_team/unser_team.component';
 import { UeberUnsComponent } from './ueber_uns/ueber_uns.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'aktuelles', component: AktuellesComponent },
-  { path: 'unser-team', component: UnserTeamComponent },
   { path: 'ueber-uns', component: UeberUnsComponent }, 
 ];
 
