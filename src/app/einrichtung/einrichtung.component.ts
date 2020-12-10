8609bcc7ff6b2607f4596ccf0c4a9b697829a8fc
Jenny <yevgeniia.shtompel@gmail.com>
2020-12-10 22:01:53 +0100

    einrichtungPage

---
+++ src/app/einrichtung/einrichtung.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-einrichtung',
+  templateUrl: './einrichtung.component.html',
+  styleUrls: ['./einrichtung.component.scss']
+})
+export class EinrichtungComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
