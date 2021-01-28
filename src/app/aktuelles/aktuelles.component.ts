90b3912abccc58b82a799f0b6314c8ea0091abaf
Jenny <yevgeniia.shtompel@gmail.com>
2021-01-28 13:46:49 +0100

    mistakeCard_Size

--- src/app/aktuelles/aktuelles.component.ts
+++ src/app/aktuelles/aktuelles.component.ts
@@ -13,7 +13,7 @@ export class AktuellesComponent implements OnInit {
   
   ngOnInit(): void {
     this.actuelles$ = of([
-      { id: "j8P9sz", title: "Pepperoni", text: "test1", date: new Date('11.12.1897'), image: 'c://temp.jpg' },
+      { id: "j8P9sz", title: "Pepperoni", text: "test1", date: new Date('11.12.1897'), image: "https://mdbootstrap.com/img/Photos/Horizontal/People/6-col/img (133).jpg" },
       { id: "tMot06", title: "Supreme", text: "test2", date: new Date(), image: 'c://temp1.jpg' },
       { id: "x9sD3g", title: "Sizzler", text: "test3", date: new Date(), image: 'c://temp2.jpg' },
       { id: "x9sD3g", title: "SizTertererzler", text: "test4", date: new Date(), image: 'c://temp4.jpg' },
